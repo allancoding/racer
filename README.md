62f8a5c43c6a3b6d483910ebe13b3d5544e79fa3
Allan Niles <allancoding.dev@gmail.com>
2023-11-17 02:42:49 +0000

    readd https support

--- README.md
+++ README.md
@@ -4,12 +4,19 @@ Reverse engineered Chrome Experiment RACER
 ## Getting Started
 
 ### Server
+If you enable https you will need to create an SSL certificate and key using the following commands:
+
+```sh
+openssl req -nodes -new -x509 -keyout ssl/server.key -out ssl/server.cert
+```
 Edit `config.json` as necessary.
 ```
 {
-    "port": "8443"
+    "port": "8443",
+    "https": false,
 }
 ```
+`port` is the port the server will listen on. `https` is a boolean value that determines whether the server will use https or not.
 
 Start the server by running:
 ```sh
--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express = require("express");
 const app = express();
 const mem = require("./mem_db.js");
-const wss = require("./ws_server.js")(app, mem);
 const config = require("./config.json");
+let server;
+if (config.https == true) {   
+    const https = require("https"); 
+    const { readFileSync } = require("fs");
+    try {
+        const server = https.createServer({
+            cert: readFileSync("./ssl/server.cert"),
+            key: readFileSync("./ssl/server.key")
+        }, app);
+    } catch (e) {
+        console.error("Could not start HTTPS server. Check your SSL certificates.");
+        console.warn("You can generate self-signed certificates with the following command:");
+        console.warn("openssl req -nodes -new -x509 -keyout ssl/server.key -out ssl/server.cert");
+        process.exit(1);
+    }
+    server = https.createServer(app);
+} else {
+    const http = require('http');
+    server = http.createServer(app);
+}
+const wss = require("./ws_server.js")(server, mem);
 app.use(require("morgan")("dev"));
 app.use(require("cors")());
 app.get('/', (req, res) => {
@@ -24,6 +44,6 @@ app.get("/control/join", (req, res) => {
         relay: 'self',
     });
 });
-app.listen(config.port, () => {
+server.listen(config.port, () => {
     console.log(`Racer server listening on port ${config.port}`)
 });
